3bd315257573ba2e75e517012145b08579cc4396
varshini-balakrishnan-ics4u1-01-25 <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-03 15:32:20 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String readFile(String filename) {
+		String result = "";
+		Scanner s = null;
+		try {
+			s = new Scanner(new BufferedReader(new FileReader(filename)));
+			while (s.hasNextLine()) {
+				result = result + s.nextLine() + "\n";
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error reading file");
+		}
+		finally {
+			if (s!= null) {
+				s.close();
+			}
+		}
+		return result;
 
 	}
 
-	
+	public static String backwardsReadFile(String filename) {
+		String text = readFile(filename);
+		String backwards = "";
+		for(int i = text.length()-1; i>=0; i--) {
+			backwards = backwards + text.charAt(i);
+		}
+		return backwards;
+	}
+	public static void main(String[] args) {
+		System.out.println(readFile("testfile.txt"));
+		System.out.println(backwardsReadFile("textfile.txt"));
+	}
 
 }
